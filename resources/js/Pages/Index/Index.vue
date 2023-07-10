25da6de3b561d830006906d6ecafdfbd993082b7
turan-98 <santanasamuel08@gmail.com>
2023-07-10 16:42:16 -0300

    upload files

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Index</div>
+    <Link rel="stylesheet" href="/hello">Show Page</Link>
+    <div>
+        The message is {{message}}
+    </div>
+</template>
+<script setup>
+import {Link}  from '@inertiajs/vue3'
+defineProps({
+    message:String
+})
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>Show</div>
+    <Link rel="stylesheet" href="/">Main Page</Link>
+</template>
+<script setup>
+import {Link} from '@inertiajs/vue3'
+</script>
